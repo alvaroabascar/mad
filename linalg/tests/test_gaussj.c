8ba8af422193d5908c687b5a4304d8e5ca615742
alvaro <alvaroabascar@gmail.com>
2015-01-02 11:07:25 +0100

    added test for singular matrix

--- linalg/tests/test_gaussj.c
+++ linalg/tests/test_gaussj.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <matrix.h>
 #include <gaussj.h>
+#include <stdlib.h>
 
-void print_a_b(matrix_double, matrix_double);
+void solve(matrix_double A, matrix_double B);
+void fill_b(matrix_double *A, matrix_double *B);
 
 int main(int argc, char *argv[])
 {
+  int i, j;
   matrix_double A, B;
   A = alloc_matrix_double(3, 3);
-  B = alloc_matrix_double(3, 2);
+  B = alloc_matrix_double(3, 1);
+  srand(100);
+  for (i = 0; i < A.nrows; i++) {
+    for (j = 0; j < A.ncols; j++) {
+      A.data[i][j] = (double) rand();
+    }
+  }
+  fill_b(&A, &B);
+
+  printf("********************************************\n");
+  printf("Trying a random system with solution 1, 2, 3\n");
+  printf("********************************************\n");
+  solve(A, B);
+
   A.data[0][0] = 1;
   A.data[0][1] = 2;
   A.data[0][2] = 3;
@@ -16,28 +32,41 @@ int main(int argc, char *argv[])
   A.data[1][1] = 5;
   A.data[1][2] = 6;
   A.data[2][0] = 7;
-  A.data[2][1] = 0;
+  A.data[2][1] = 8;
   A.data[2][2] = 9;
 
-  B.data[0][0] = 14;
-  B.data[1][0] = 32;
-  B.data[2][0] = 34;
-  B.data[0][1] = 28;
-  B.data[1][1] = 64;
-  B.data[2][1] = 68;
+  fill_b(&A, &B);
 
-  printf("before gaussj:\n");
-  print_a_b(A, B);
-  gaussj(&A, &B);
-  printf("after gaussj:\n");
-  print_a_b(A, B);
+  printf("\n*****************************\n");
+  printf("Trying with a singular matrix\n");
+  printf("*****************************\n");
+  solve(A, B);
+
   return 0;
 }
 
-void print_a_b(matrix_double A, matrix_double B)
+void solve(matrix_double A, matrix_double B)
 {
+  printf("before gaussj:\n");
   printf("A:\n");
   print_matrix_double(A);
   printf("B:\n");
   print_matrix_double(B);
+  if (! gaussj(&A, &B) ) {
+    printf("after gaussj:\n");
+    printf("A:\n");
+    print_matrix_double(A);
+    printf("B:\n");
+    print_matrix_double(B);
+  }
+}
+
+void fill_b(matrix_double *A, matrix_double *B)
+{
+  int i, j;
+  for (i = 0; i < A->nrows; i++) {
+    for (j = 0; j < A->ncols; j++) {
+      B->data[i][0] += A->data[i][j] * (j + 1);
+    }
+  }
 }
