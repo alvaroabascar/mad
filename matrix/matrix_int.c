93598a924c68e3b9c8902ae469e5861c2a60a3df
alvaro <alvaroabascar@gmail.com>
2015-01-04 13:04:55 +0100

    updated matrix_int copying matrix_double

--- matrix/matrix_int.c
+++ matrix/matrix_int.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "matrix_int.h"
-
 #define max(x) ((x > 0) ? x : -x)
 
 /* allocate enough space for a nrows by ncols matrix and return it */
@@ -21,15 +20,17 @@ matrix_int alloc_matrix_int(size_t nrows, size_t ncols)
 }
 
 /* free the space used by the matrix */
-void free_matrix_int(matrix_int *matrix)
+void free_matrix_int(matrix_int matrix)
 {
   int i;
   /* free each row */
-  for (i = 0; i < matrix->nrows; i++) {
-    free(matrix->data[i]);
+  for (i = 0; i < matrix.nrows; i++) {
+    free(matrix.data[i]);
   }
   /* free data */
-  free(matrix->data);
+  free(matrix.data);
+  matrix.nrows = 0;
+  matrix.ncols = 0;
 }
 
 matrix_int copy_matrix_int(matrix_int matrix)
@@ -60,10 +61,20 @@ void print_matrix_int(matrix_int matrix)
   }
 }
 
+void print_array_int(int len, int *array)
+{
+  int i;
+  printf("{");
+  for (i = 0; i < len; i++) {
+    printf("%10.4f%s", array[i], (i < len - 1) ? ", " : "}\n");
+  }
+}
+
+
 /* Given a matrix and two coordinates "start" and "end", multiply by k all the
  * elements in the matrix within the square delimited by "start" and "end"
  */
-void multiply_section_matrix_int(matrix_int *matrix,
+void multiply_section_matrix_int(matrix_int matrix,
                                   struct pair_coordinates section,
                                   int k)
 {
@@ -73,7 +84,7 @@ void multiply_section_matrix_int(matrix_int *matrix,
   end = section.b;
   for (i = start.row; i <= end.row; i++) {
     for (j = start.col; j <= end.col; j++) {
-      matrix->data[i][j] *= k;
+      matrix.data[i][j] *= k;
     }
   }
 }
@@ -81,7 +92,7 @@ void multiply_section_matrix_int(matrix_int *matrix,
 /* Given a matrix and two coordinates "start" and "end", add k to all the
  * elements in the matrix within the square delimited by "start" and "end"
  */
-void add_to_section_matrix_int(matrix_int *matrix,
+void add_to_section_matrix_int(matrix_int matrix,
                                   struct pair_coordinates section,
                                   int k)
 {
@@ -91,7 +102,7 @@ void add_to_section_matrix_int(matrix_int *matrix,
   end = section.b;
   for (i = start.row; i <= end.row; i++) {
     for (j = start.col; j <= end.col; j++) {
-      matrix->data[i][j] += k;
+      matrix.data[i][j] += k;
     }
   }
 }
@@ -104,15 +115,15 @@ void interchange_array_elements_int(int *array, int i, int j)
   array[j] = tmp;
 }
 
-void reorder_matrix_rows_int(matrix_int *matrix, int *orders_arg)
+void reorder_matrix_rows_int(matrix_int matrix, int *orders_arg)
 {
   int i;
-  int orders[matrix->nrows];
+  int orders[matrix.nrows];
   /* make copy of orders_arg to avoid destroying the original */
-  for (i = 0; i < matrix->nrows; i++) {
+  for (i = 0; i < matrix.nrows; i++) {
     orders[i] = orders_arg[i];
   }
-  for (i = 0; i < matrix->nrows; i++) {
+  for (i = 0; i < matrix.nrows; i++) {
     while (orders[i] != i) {
       interchange_rows_matrix_int(matrix, i, orders[i]);
       interchange_array_elements_int(orders, i, orders[i]);
@@ -120,24 +131,40 @@ void reorder_matrix_rows_int(matrix_int *matrix, int *orders_arg)
   }
 }
 
-/* multiply a row (row) by a int (k) */
-void multiply_row_matrix_int(matrix_int *matrix, int row, int k)
+void reorder_array_int(int len, int *array, int *orders_arg)
 {
   int i;
-  for (i = 0; i < matrix->ncols; i++) {
-    matrix->data[row][i] *= k;
+  int orders[len];
+  /* copy orders array to avoid destroying it */
+  for (i = 0; i < len; i++) {
+    orders[i] = orders_arg[i];
+  }
+  for (i = 0; i < len; i++) {
+    while (orders[i] != i) {
+      interchange_array_elements_int(array, i, orders[i]);
+      interchange_array_elements_int(orders, i, orders[i]);
+    }
+  }
+}
+
+/* multiply a row (row) by a int (k) */
+void multiply_row_matrix_int(matrix_int matrix, int row, int k)
+{
+  int i;
+  for (i = 0; i < matrix.ncols; i++) {
+    matrix.data[row][i] *= k;
   }
 }
 
 /* multiply_matrix_int:
  * take a matrix "matrix" and divide all its elements by "k"
  */
-void multiply_matrix_int(matrix_int *matrix, int k)
+void multiply_matrix_int(matrix_int matrix, int k)
 {
   int i, j;
-  for (i = 0; i < matrix->nrows; i++) {
-    for (j = 0; j < matrix->ncols; j++) {
-      matrix->data[i][j] *= k;
+  for (i = 0; i < matrix.nrows; i++) {
+    for (j = 0; j < matrix.ncols; j++) {
+      matrix.data[i][j] *= k;
     }
   }
 }
@@ -146,48 +173,48 @@ void multiply_matrix_int(matrix_int *matrix, int k)
  * given a matrix "A", a vector "v" and an integer "row",
  * copy the row A[row] into v
  */
-void copy_row_matrix_int(matrix_int *A, int *v, int row)
+void copy_row_matrix_int(matrix_int A, int *v, int row)
 {
   int i;
-  for (i = 0; i < A->ncols; i++) {
-    v[i] = A->data[row][i];
+  for (i = 0; i < A.ncols; i++) {
+    v[i] = A.data[row][i];
   }
 }
 
 /* add "vector" to row matrix[row] */
-void add_to_row_matrix_int(matrix_int *matrix, int row,
-                                      int vector[matrix->ncols])
+void add_to_row_matrix_int(matrix_int matrix, int row,
+                                      int vector[matrix.ncols])
 {
   int i;
-  for (i = 0; i < matrix->ncols; i++) {
-    matrix->data[row][i] += vector[i];
+  for (i = 0; i < matrix.ncols; i++) {
+    matrix.data[row][i] += vector[i];
   }
 }
 
 /* interchange_rows_matrix_int:
  * take a matrix and interchange rowA and rowB
  */
-void interchange_rows_matrix_int(matrix_int *matrix,
+void interchange_rows_matrix_int(matrix_int matrix,
                                         int rowA, int rowB)
 {
   int *tmp;
-  tmp = matrix->data[rowA];
-  matrix->data[rowA] = matrix->data[rowB];
-  matrix->data[rowB] = tmp;
+  tmp = matrix.data[rowA];
+  matrix.data[rowA] = matrix.data[rowB];
+  matrix.data[rowB] = tmp;
 }
 
 /* interchange_cols_matrix_int:
  * take a matrix and interchange colA and colB
  */
-void interchange_cols_matrix_int(matrix_int *matrix,
+void interchange_cols_matrix_int(matrix_int matrix,
                                         int colA, int colB)
 {
   int i;
   int tmp;
-  for (i = 0; i < matrix->ncols; i++) {
-    tmp = matrix->data[i][colA];
-    matrix->data[i][colA] = matrix->data[i][colB];
-    matrix->data[i][colB] = tmp;
+  for (i = 0; i < matrix.ncols; i++) {
+    tmp = matrix.data[i][colA];
+    matrix.data[i][colA] = matrix.data[i][colB];
+    matrix.data[i][colB] = tmp;
   }
 }
 
--- matrix/matrix_int.h
+++ matrix/matrix_int.h
@@ -11,23 +11,25 @@ typedef struct {
 
 matrix_int alloc_matrix_int(size_t nrows, size_t ncols);
 matrix_int copy_matrix_int(matrix_int matrix);
-void free_matrix_int(matrix_int *matrix);
+void free_matrix_int(matrix_int matrix);
 void print_matrix_int(matrix_int matrix);
-void multiply_section_matrix_int(matrix_int *matrix,
+void print_array_int(int len, int *array);
+void multiply_section_matrix_int(matrix_int matrix,
                                   struct pair_coordinates section,
                                   int k);
-void add_to_section_matrix_int(matrix_int *matrix,
+void add_to_section_matrix_int(matrix_int matrix,
                                   struct pair_coordinates section,
                                   int k);
 void interchange_array_elements_int(int *array, int i, int j);
-void reorder_matrix_rows_int(matrix_int *matrix, int *orders);
-void multiply_row_matrix_int(matrix_int *matrix, int row, int k);
-void multiply_matrix_int(matrix_int *matrix, int k);
-void copy_row_matrix_int(matrix_int *A, int *v, int row);
-void add_to_row_matrix_int(matrix_int *matrix, int row,
-                                      int vector[matrix->ncols]);
-void interchange_rows_matrix_int(matrix_int *matrix, int rowA, int rowB);
-void interchange_cols_matrix_int(matrix_int *matrix, int colA, int colB);
+void reorder_matrix_rows_int(matrix_int matrix, int *orders);
+void reorder_array_int(int len, int *array, int *orders);
+void multiply_row_matrix_int(matrix_int matrix, int row, int k);
+void multiply_matrix_int(matrix_int matrix, int k);
+void copy_row_matrix_int(matrix_int A, int *v, int row);
+void add_to_row_matrix_int(matrix_int matrix, int row,
+                              int vector[matrix.ncols]);
+void interchange_rows_matrix_int(matrix_int matrix, int rowA, int rowB);
+void interchange_cols_matrix_int(matrix_int matrix, int colA, int colB);
 void copy_vector_int(int len, int *source, int *dest);
 void multiply_vector_int(int len, int *vector, int k);
 int absmax_vector_int(int len, int *vector);
