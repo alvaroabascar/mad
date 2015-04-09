47aa2cd4de8d31c5df624d7455ac14c9c1da3968
alvaro <alvaroabascar@gmail.com>
2015-04-09 12:58:31 +0200

    extract_matrix_double extracts a section INCLUDING the limits

--- matrix/matrix_double.c
+++ matrix/matrix_double.c
@@ -119,11 +119,12 @@ matrix_double extract_section_matrix_double(matrix_double matrix,
                                             struct pair_coordinates section)
 {
   int nrows, ncols, i, j;
-  nrows = section.b.row - section.a.row;
-  ncols = section.b.col - section.a.col;
+  /* from b.row to a.row INCLUSIVE, and the same for the columns */
+  nrows = section.b.row - section.a.row + 1;
+  ncols = section.b.col - section.a.col + 1;
   matrix_double new_matrix = alloc_matrix_double(nrows, ncols);
-  for (i = section.a.row; i < section.b.row; i++) {
-    for (j = section.a.col; j < section.b.col; j++) {
+  for (i = section.a.row; i <= section.b.row; i++) {
+    for (j = section.a.col; j <= section.b.col; j++) {
       new_matrix.data[i-section.a.row][j-section.a.col] = matrix.data[i][j];
     }
   }
